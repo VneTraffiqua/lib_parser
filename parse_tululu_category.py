e9a7aebcd5cb04734c5c425bccb919dd96369a2c
VneTraffiqua <safe72again@gmail.com>
2022-11-21 01:48:10 -0800

    Update error handler

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -4,6 +4,7 @@ import argparse
 import re
 import time
 import json
+import sys
 from bs4 import BeautifulSoup
 import urllib.parse
 from urllib.error import URLError
@@ -67,55 +68,63 @@ def main():
     Path(f'{args.dest_folder}/books').mkdir(parents=True, exist_ok=True)
     Path(f'{args.dest_folder}/img').mkdir(parents=True, exist_ok=True)
     all_books_params = []
+    page = args.start_page
     if args.last_page:
         last_page = args.last_page
     else:
-        last_page = args.start_page + 1
-    for page in range(args.start_page, last_page):
-        url = f'https://tululu.org/l55/{page}'
-        response = requests.get(url)
-        response.raise_for_status()
-        check_for_redirect(response)
-        soup = BeautifulSoup(response.text, 'lxml')
-        all_books_path = get_books_url(soup)
-        for book_path in all_books_path:
-            try:
-                book_url = urllib.parse.urljoin(
-                    'https://tululu.org/',
-                    book_path
-                )
-                response = requests.get(book_url)
-                response.raise_for_status()
-                check_for_redirect(response)
-                soup = BeautifulSoup(response.text, 'lxml')
-                book_img_url, book_title, book_author, \
-                    genres, comments_texts = get_book_params(soup)
-                if not args.skip_imgs:
-                    img_src = download_book_img(book_url, book_img_url)
-                book_id = re.sub(r"[^\d\.]", "", book_path)
-                if not args.skip_txt:
-                    book_path = download_txt_book(book_id, book_title)
+        last_page = page + 1
 
-                book_params = {
-                    'title': book_title,
-                    'Author': book_author,
-                    'img_src': os.path.relpath(img_src),
-                    'book_path': os.path.relpath(book_path),
-                    'genres': genres,
-                    'comments': comments_texts,
-                }
-                all_books_params.append(book_params)
-            except URLError:
-                continue
-            except requests.exceptions.HTTPError:
-                print('HTTP error...')
-                continue
-            except requests.exceptions.ConnectionError:
-                time.sleep(3)
-                continue
-    books_json = json.dumps(all_books_params, ensure_ascii=False)
-    with open(f'{args.json_path}books.json', 'w') as file:
-        file.write(books_json)
+    while page <= last_page:
+        try:
+            url = f'https://tululu.org/l55/{page}'
+            response = requests.get(url)
+            response.raise_for_status()
+            check_for_redirect(response)
+            soup = BeautifulSoup(response.text, 'lxml')
+            all_books_path = get_books_url(soup)
+            for book_path in all_books_path:
+                try:
+                    book_url = urllib.parse.urljoin(
+                        'https://tululu.org/',
+                        book_path
+                    )
+                    response = requests.get(book_url)
+                    response.raise_for_status()
+                    check_for_redirect(response)
+                    soup = BeautifulSoup(response.text, 'lxml')
+                    book_img_url, book_title, book_author, \
+                        genres, comments_texts = get_book_params(soup)
+                    if not args.skip_imgs:
+                        img_src = download_book_img(book_url, book_img_url)
+                    book_id = re.sub(r"[^\d\.]", "", book_path)
+                    if not args.skip_txt:
+                        book_path = download_txt_book(book_id, book_title)
+
+                    book_params = {
+                        'title': book_title,
+                        'Author': book_author,
+                        'img_src': os.path.relpath(img_src),
+                        'book_path': os.path.relpath(book_path),
+                        'genres': genres,
+                        'comments': comments_texts,
+                    }
+                    all_books_params.append(book_params)
+                except URLError:
+                    continue
+        except URLError:
+            page += 1
+            continue
+        except requests.exceptions.HTTPError:
+            print('HTTP error...')
+            page += 1
+            continue
+        except requests.exceptions.ConnectionError:
+            print('Failed connection..', file=sys.stderr)
+            time.sleep(3)
+            continue
+        books_json = json.dumps(all_books_params, ensure_ascii=False)
+        with open(f'{args.json_path}books.json', 'w') as file:
+            file.write(books_json)
 
 
 if __name__ == '__main__':
