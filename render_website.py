541a9a06882ce445fc9ab51a964b061ed2b83f05
VneTraffiqua <safe72again@gmail.com>
2022-12-12 23:10:38 -0800

    Split books by two columns.

--- render_website.py
+++ render_website.py
@@ -2,7 +2,7 @@ from http.server import HTTPServer, SimpleHTTPRequestHandler
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from livereload import Server
 import json
-
+from more_itertools import chunked
 
 def on_reload():
     with open('books.json', 'r') as file:
@@ -12,9 +12,10 @@ def on_reload():
         loader=FileSystemLoader('./'),
         autoescape=select_autoescape(['html', 'xml'])
     )
+    grouped_books = list(chunked(books, 2))
     template = env.get_template('template.html')
     rendered_page = template.render(
-        books=books
+        grouped_books=grouped_books
     )
     with open('index.html', 'w', encoding="utf8") as file:
         file.write(rendered_page)
