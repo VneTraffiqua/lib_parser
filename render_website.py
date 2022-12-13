b940b20682d018d92067b46cd69345173edc7530
VneTraffiqua <safe72again@gmail.com>
2022-12-13 06:04:35 -0800

    Add paginator

--- render_website.py
+++ render_website.py
@@ -4,6 +4,7 @@ from jinja2 import Environment, FileSystemLoader, select_autoescape
 from livereload import Server
 import json
 from more_itertools import chunked
+from pathlib import Path
 
 
 def on_reload():
@@ -14,15 +15,21 @@ def on_reload():
         loader=FileSystemLoader('./'),
         autoescape=select_autoescape(['html', 'xml'])
     )
-    os.makedirs('./pages')
+    os.makedirs('./pages', exist_ok=True)
     books_pages = list(chunked(books, 20))
     for num, books_on_page in enumerate(books_pages, 1):
         grouped_books = list(chunked(books_on_page, 2))
         template = env.get_template('template.html')
         rendered_page = template.render(
-            grouped_books=grouped_books
+            number_of_page=num,
+            grouped_books=grouped_books,
+            count_pages=len(books_pages)
         )
-        with open(f'./pages/index{num}.html', 'w', encoding="utf8") as file:
+        with open(
+                Path.cwd() / 'pages' / f'index{num}.html',
+                'w',
+                encoding="utf8",
+        ) as file:
             file.write(rendered_page)
 
 
@@ -31,7 +38,7 @@ def main():
 
     server = Server()
     server.watch('./template.html', on_reload)
-    server.serve(root='.')
+    server.serve(root='.', default_filename='pages/index1.html')
 
     # server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
     # server.serve_forever()
